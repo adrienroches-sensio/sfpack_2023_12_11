93029edbbba7fd8abb79010f3be50f6ab35c2437
Adrien Roches <adrien@roc-it.tech>
2023-12-11 12:43:34 +0100

    [sfstart] 02-test : Update test to use data provider

--- tests/Controller/HelloControllerTest.php
+++ tests/Controller/HelloControllerTest.php
@@ -2,14 +2,27 @@
 
 namespace App\Tests\Controller;
 
+use Generator;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HelloControllerTest extends WebTestCase
 {
-    public function testHelloWithDefaultName(): void
+    public static function provideValidHelloUris(): Generator
+    {
+        yield 'default' => ['/hello'];
+        yield 'name without "-"' => ['/hello/Plop'];
+        yield 'name having only one "-"' => ['/hello/Jean-Pierre'];
+    }
+
+    /**
+     * @dataProvider provideValidHelloUris
+     *
+     * @group smoke-test
+     */
+    public function testHelloUri(string $uri): void
     {
         $client = static::createClient();
-        $client->request('GET', '/hello');
+        $client->request('GET', $uri);
 
         $this->assertResponseIsSuccessful();
     }
