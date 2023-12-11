96f3660249909d0e558141d0e956affb7f059a6e
Adrien Roches <adrien@roc-it.tech>
2023-12-11 16:16:55 +0100

    [sfstart] 03-twig : Inject movies in navbar controller

--- src/Controller/NavbarController.php
+++ src/Controller/NavbarController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Model\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class NavbarController extends AbstractController
 {
-    public function main(): Response
+    public function main(MovieRepository $movieRepository): Response
     {
-        return $this->render('navbar.html.twig');
+        return $this->render('navbar.html.twig', [
+            'movies' => $movieRepository->listAll(),
+        ]);
     }
 }
