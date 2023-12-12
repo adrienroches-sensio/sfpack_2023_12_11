0065be90d5d693a8444ceb42e10c52ea0c9cde8c
Adrien Roches <adrien@roc-it.tech>
2023-12-12 16:09:47 +0100

    [sfstart] 06-form : Move poster constraint to a dedicated one using Compound

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Model\Movie as MovieModel;
 use App\Repository\MovieRepository;
 use App\Validator\Constraints\MoviePosterExists;
+use App\Validator\Constraints\PosterValid;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
@@ -43,10 +44,7 @@ class Movie
     private ?\DateTimeImmutable $releasedAt = null;
 
     #[ORM\Column(length: 255)]
-    #[Assert\AtLeastOneOf(constraints: [
-        new MoviePosterExists(),
-        new Assert\Url(),
-    ])]
+    #[PosterValid()]
     private ?string $poster = null;
 
     #[ORM\ManyToMany(targetEntity: Genre::class, inversedBy: 'movies')]
