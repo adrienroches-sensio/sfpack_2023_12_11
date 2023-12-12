e40431fee7679175d3cfcc4b78c7e6aa424df803
Adrien Roches <adrien@roc-it.tech>
2023-12-12 14:01:31 +0100

    [sfstart] 06-form : Generate Movietype

---
+++ src/Form/MovieType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use App\Entity\Movie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MovieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('slug')
+            ->add('title')
+            ->add('plot')
+            ->add('releasedAt')
+            ->add('poster')
+            ->add('genres', EntityType::class, [
+                'class' => Genre::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Movie::class,
+        ]);
+    }
+}
