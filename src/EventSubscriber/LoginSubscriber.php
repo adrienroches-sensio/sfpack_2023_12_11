86697c82893b22db483534a60a77e8d74c80e60f
Adrien Roches <adrien@roc-it.tech>
2023-12-14 14:10:16 +0100

    [sfmaster] 10-event : Implement LoginSubscriber

--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -2,20 +2,40 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Clock\ClockInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
-    public function onLoginSuccessEvent($event): void
+    public function __construct(
+        private readonly ClockInterface $clock,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    public function updateLastLoggedIn(LoginSuccessEvent $event): void
     {
-        // ...
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setLastLoggedInAt($this->clock->now());
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            LoginSuccessEvent::class => 'onLoginSuccessEvent',
+            LoginSuccessEvent::class => [
+                ['updateLastLoggedIn', 0]
+            ],
         ];
     }
 }
